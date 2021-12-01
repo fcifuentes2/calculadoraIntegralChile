3c5c005e6c6a543a1fb6fc237b2f8afaeb728920
fernando <fcifuentes2@misena.edu.co>
2021-12-01 11:20:08 -0500

    se agregaron extras, deco adicional, recargo mora, tnt flexible

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
+
+// carga la funcion despues de que se cargue por completo el documento
 window.onload = function(){
     addEventListener('change', obtenerTotal);
 }
 // buscar el <form> y guardarlo en una variable
-
 const form = document.querySelector('form');
 
 // decir que en el evento enviar se sume el total
 form.addEventListener('change', obtenerTotal);
-// form.addEventListener('submit', obtenerTotal);
+
 
 function obtenerTotal(event) {
+
 // evitar enviar el formulario que ocasiona que se refresque la pagina
 event.preventDefault();
 
-// obtener todos los input radio
-// const formData = new FormData(event.target);
-
-// convertir los valores a String, o colocar un 0 si no elegieron algo
+// obtener todos los input radio y checkbox
 const planRadio = document.querySelector('input[name=plan]:checked')
 const rvoRadio = document.querySelector('input[name=rvo]:checked')
 const equiposRadio = document.querySelector('input[name=equipos]:checked')
@@ -32,6 +31,9 @@ const premium9Check = document.querySelector('input[name=premium9]:checked')
 const premium10Check = document.querySelector('input[name=premium10]:checked')
 const premium11Check = document.querySelector('input[name=premium11]:checked')
 const premium12Check = document.querySelector('input[name=premium12]:checked')
+const recargoCheck = document.querySelector('input[name=recargo]:checked')
+const tntCheck = document.querySelector('input[name=tntflex]:checked')
+const decoCheck = document.querySelector('input[name=deco]:checked')
 const rvp1Check = document.querySelector('input[name=rvp1]:checked')
 const rvp2Check = document.querySelector('input[name=rvp2]:checked')
 const rvp3Check = document.querySelector('input[name=rvp3]:checked')
@@ -42,6 +44,7 @@ const rvp7Check = document.querySelector('input[name=rvp7]:checked')
 const rvp8Check = document.querySelector('input[name=rvp8]:checked')
 const rvoCheck = document.querySelector('input[name=rvoEquipo]:checked')          
 
+// convertir los valores a String, o colocar un 0 si no elegieron algo
 const plan = planRadio ?  Number(planRadio.value): 0
 const rvo = rvoRadio ?  Number(rvoRadio.value): 0
 const equipos = equiposRadio ?  Number(equiposRadio.value): 0
@@ -57,6 +60,9 @@ const premium9 = premium9Check ?  Number(premium9Check.value): 0
 const premium10 = premium10Check ?  Number(premium10Check.value): 0
 const premium11 = premium11Check ?  Number(premium11Check.value): 0
 const premium12 = premium12Check ?  Number(premium12Check.value): 0
+const recargo = recargoCheck ?  Number(recargoCheck.value): 0
+const tntflex = tntCheck ?  Number(tntCheck.value): 0
+const deco = decoCheck ?  Number(decoCheck.value): 0 
 const rvp1 = rvp1Check ?  Number(rvp1Check.value): 0
 const rvp2 = rvp2Check ?  Number(rvp2Check.value): 0
 const rvp3 = rvp3Check ?  Number(rvp3Check.value): 0
@@ -66,10 +72,13 @@ const rvp6 = rvp6Check ?  Number(rvp6Check.value): 0
 const rvp7 = rvp7Check ?  Number(rvp7Check.value): 0
 const rvp8 = rvp8Check ?  Number(rvp8Check.value): 0
 const rvoEquipo = rvoCheck ?  Number(rvoCheck.value): 0
-        
+
+//suma total se le asigna a variable result
 const result = plan + rvo + equipos + premium1 + premium2 + premium3 + premium4 + 
 premium5 + premium6 + premium7 + premium8 + premium9 + premium10 + premium11 +
-premium12 + rvp1 + rvp2 + rvp3 + rvp4 + rvp5 + rvp6 + rvp7 + rvp8 + rvoEquipo;
+premium12 + rvp1 + rvp2 + rvp3 + rvp4 + rvp5 + rvp6 + rvp7 + rvp8 + rvoEquipo + recargo + tntflex + deco;
+
+//se asigna valor de variable result a resultado para poder ser mostrado en pantalla
 document.getElementById('resultado').innerHTML = result;
 
 }
