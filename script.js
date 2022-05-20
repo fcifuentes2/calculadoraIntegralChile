78e7a84eeb4180182b40b27ad8c5db63c6fc5c20
fcifuentes2 <fcifuentes2@misena.edu.co>
2022-05-20 14:59:56 -0500

    plantilla contactos

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // carga la funcion despues de que se cargue por completo el documento
 window.onload = function(){
     addEventListener('change', obtenerTotal);
+    addEventListener('change', Total);
 }
 // buscar el <form> y guardarlo en una variable
 const form = document.querySelector('form');
@@ -97,5 +98,100 @@ rvp6 + rvp7 + rvp8 + rvp9 + rvoEquipo - rvoExtra + Extra + recargo + tntflex + d
 
 //se asigna valor de variable result a resultado para poder ser mostrado en pantalla
 document.getElementById('resultado').innerHTML = result;
-
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('marcarTodo').addEventListener('click', function(e) {
+        e.preventDefault();
+        //seleccionarTodo();
+        checkAll();
+    });
+    document.getElementById('desmarcarTodo').addEventListener('click', function(e) {
+        e.preventDefault();
+        //desmarcarTodo();
+        uncheckAll();
+    });
+});
+
+function seleccionarTodo() {
+    for (let i=0; i < document.f1.elements.length; i++) {
+        if(document.f1.elements[i].type === "checkbox") {
+            document.f1.elements[i].checked = true;
+        }
+    }
+}
+
+function desmarcarTodo() {
+    for (let i=0; i<document.f1.elements.length; i++) {
+        if(document.f1.elements[i].type == "checkbox") {
+            document.f1.elements[i].checked = false
+        }
+    }
+}
+
+function checkAll() {
+    document.querySelectorAll('#formElement input[type=checkbox]').forEach(function(checkElement) {
+        checkElement.checked = true;
+    });
+}
+
+function uncheckAll() {
+    document.querySelectorAll('#formElement input[type=checkbox]').forEach(function(checkElement) {
+        checkElement.checked = false;
+    });
+}
+
+
+addEventListener('change', Total);
+
+function Total() {
+
+// obtener todos los input radio y checkbox
+const rvo0Check = document.querySelector('input[name=rvo0]:checked')
+const rvo01Check = document.querySelector('input[name=rvo01]:checked')
+const rvo02Check = document.querySelector('input[name=rvo02]:checked')
+const rvp0Check = document.querySelector('input[name=rvp0]:checked')
+const rvp01Check = document.querySelector('input[name=rvp01]:checked')
+const facCheck = document.querySelector('input[name=fac]:checked')
+const fac1Check = document.querySelector('input[name=fac1]:checked')
+const proCheck = document.querySelector('input[name=pro]:checked')
+const pro1Check = document.querySelector('input[name=pro1]:checked')
+const pro2Check = document.querySelector('input[name=pro2]:checked')
+const pro3Check = document.querySelector('input[name=pro3]:checked')
+const pro4Check = document.querySelector('input[name=pro4]:checked')
+const tstCheck = document.querySelector('input[name=tst]:checked')
+const tst1Check = document.querySelector('input[name=tst1]:checked')
+const tst2Check = document.querySelector('input[name=tst2]:checked')
+const tst3Check = document.querySelector('input[name=tst3]:checked')
+const tst4Check = document.querySelector('input[name=tst4]:checked')
+const tst5Check = document.querySelector('input[name=tst5]:checked')
+// convertir los valores a String, o colocar un 0 si no elegieron algo//
+const rvo0 = rvo0Check ?  rvo0Check.value: ""
+const rvo01 = rvo01Check ?  rvo01Check.value: ""
+const rvo02 = rvo02Check ?  rvo02Check.value: ""
+const rvp0 = rvp0Check ?  rvp0Check.value: ""
+const rvp01 = rvp01Check ?  rvp01Check.value: ""
+const fac = facCheck ?  facCheck.value: ""
+const fac1 = fac1Check ?  fac1Check.value: ""
+const pro = proCheck ?  proCheck.value: ""
+const pro1 = pro1Check ?  pro1Check.value: ""
+const pro2 = pro2Check ?  pro2Check.value: ""
+const pro3 = pro3Check ?  pro3Check.value: ""
+const pro4 = pro4Check ?  pro4Check.value: ""
+const tst = tstCheck ?  tstCheck.value: ""
+const tst1 = tst1Check ?  tst1Check.value: ""
+const tst2 = tst2Check ?  tst2Check.value: ""
+const tst3 = tst3Check ?  tst3Check.value: ""
+const tst4 = tst4Check ?  tst4Check.value: ""
+const tst5 = tst5Check ?  tst5Check.value: ""
+
+//suma total se le asigna a variable result
+const resultado = rvo0 + rvo01 + rvo02 + rvp0 + rvp01 + fac + fac1 + pro + pro1 + pro2 + pro3 + pro4 + tst + tst1 + tst2 + tst3 + tst4 + tst5;
+
+//se asigna valor de variable result a resultado para poder ser mostrado en pantalla
+document.getElementById('texto').innerHTML = resultado;
+}
+
+
+
+        
